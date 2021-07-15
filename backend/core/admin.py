4b9fa5aa721b73f79d7423950092ea711faf52dd
Vinicius Perrone <perronevinicius2018@gmail.com>
2021-07-14 20:35:13 -0400

    added cors headers

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from models import Products
 
 # Register your models here.
+
+@admin.register(Products)
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'description', 'price']
+
+
