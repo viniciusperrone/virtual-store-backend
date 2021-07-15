499d3fe7b191418365703c7f067f8f3d36d9e874
Vinicius Perrone <perronevinicius2018@gmail.com>
2021-07-14 20:49:06 -0400

    added serializers

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,10 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    'corsheaders'
+    'corsheaders',
+    'rest_framework'
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleWare',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
---
+++ backend/core/serialize.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import Products
+
+class ProductsSerialize(serializers.ModelSerializer):
+    class Meta:
+        model = Products
+        fields = (  
+                    'name',
+                    'description'
+                    'price'
+                )
